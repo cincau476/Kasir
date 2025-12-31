04a1a693b39e1dfee75ee6ceec2c01e0b9eec608
virrel <virrelj@gmail.com>
2025-12-31 14:08:20 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 1. Jangan lupa import ini
 import './index.css'
 import App from './App.jsx'
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 2. Bungkus App dengan BrowserRouter dan set basename */}
+    <BrowserRouter basename="/kasir">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
